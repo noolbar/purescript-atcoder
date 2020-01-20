6a1f60d5caaf0c45dfea639ce051c38ea9bfc50c
nb <nb@debian.debian>
2020-01-20 12:14:06 -0500

    update

--- src/ABC149B.purs
+++ src/ABC149B.purs
@@ -2,16 +2,17 @@ module ABC149B where
   
 import Prelude
 
+import Data.Array as Array
 import Data.BigInt (BigInt)
-import Data.BigInt as BigInt
-import Data.Foldable (foldr)
+import Data.BigInt as BI
+import Data.Maybe as Maybe
 import Data.String as String
 import Effect (Effect)
-import Node.Encoding (Encoding)
 import Node.Encoding as Encoding
 import Node.FS.Sync as FS
 import Node.Process as Process
 import Node.Stream as Stream
+import Partial.Unsafe as Unsafe
 
 
 main :: Effect Unit
@@ -29,20 +30,20 @@ main = do
         doMatch :: Array BigInt -> Array BigInt
         doMatch [a, b, k]
           | a + b > k 
-            , a > k = {a: (a - k), b}
+            , a > k = [(a - k), b]
           | a + b > k 
-            , a < k = {a: 0, b:(a + b - k)}
-          | a + b < k = {a:0, b:0}
-          | otherwise = {a:0, b:0}
-      in do
-        {a, b, k} <- readbint input
-        show $ foldr (<>) " " (doMatch [a, b, k])
+             = [b0, (a + b - k)]
+          | a + b < k = [b0, b0]
+          | otherwise = [b0, b0]
+        doMatch _ = [b0, b0]
+        b0 :: BigInt
+        b0 = zero :: BigInt -- Unsafe.unsafePartial (Maybe.fromJust (BI.fromNumber 0.0))
+        ao = Unsafe.unsafePartial (Maybe.fromJust $ Array.head $ doMatch (readbint input))
+        bo = Unsafe.unsafePartial (Maybe.fromJust $ Array.last $ doMatch (readbint input))
+      in
+        show $ "  " <> (BI.toString ao) <> " " <> (BI.toString bo) <> "\n"
     
     readbint :: String -> Array BigInt
-    readbint input = do
-      strs <- splitBySp input
-      map BigInt.fromString strs
-      
-
-
+    readbint input =
+      map (\s -> Unsafe.unsafePartial (Maybe.fromJust(BI.fromString s))) (splitBySp input)
 
